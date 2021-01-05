b06cbfe604e589455b339d92361f382c6b66fccc
arturos <arturos@cern.ch>
2021-01-05 04:49:31 +0100

    Run

--- Analysis/ZZDiBosonAnalysis/main_ZZDiBosonAnalysis.C
+++ Analysis/ZZDiBosonAnalysis/main_ZZDiBosonAnalysis.C
@@ -9,12 +9,12 @@ void main_ZZDiBosonAnalysis(int proof = 0, int option= 0)
 {
   // path to your local directory *or* URL, please change the default one!
   /* Local path example */
-  //TString path  = "/eos/project/a/atlas-outreach/projects/open-data/OpenDataTuples/renamedLargeRJets/4lep/";
-  //TString path2 = "/eos/project/a/atlas-outreach/projects/open-data/OpenDataTuples/renamedLargeRJets/2lep/";
+  TString path  = "/media/sf_compartida/4lep/";
+  TString path2 = "/media/sf_compartida/2lep/";
 
-  /* The URL to the ATLAS Open Data website repository */
+  /* The URL to the ATLAS Open Data website repository
   TString path  = "http://www.universidad.ch/ATLAS/outreach/open-data-2020/4lep/";
-  TString path2 = "http://www.universidad.ch/ATLAS/outreach/open-data-2020/2lep/";
+  TString path2 = "http://www.universidad.ch/ATLAS/outreach/open-data-2020/2lep/";*/
 
   //***************************************************************************************************//
   // adding chains of all MC and data samples
